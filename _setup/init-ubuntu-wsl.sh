e4b9bb939e00a38aa59b22c5624d3a4c53e15222
Pulsh <contact@pul.sh>
2024-06-19 14:13:54 -0400

    Initial commit

---
+++ _setup/init-ubuntu-wsl.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+# Shell Script to automatically setup & configure WSL Ubuntu
+
+sudo su
+
+# Setup Variables:
+
+# Add directory: ~/Development/ 
+cd ~
+mkdir Development
+cd Development/
+
